3ca0471a72d743c602f3dd03474b2378e06120be
Will Holcomb <dys@dhappy.org>
2021-05-01 22:38:17 -0400

    addin' Chakra & images 🥈

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
-import { createGlobalStyle } from "styled-components";
-import FileReaderInput from "react-file-reader-input";
-import { ReactReader } from "./modules";
+import React, { Component } from 'react'
+import FileReaderInput from 'react-file-reader-input'
+import { ReactReader } from './modules'
 import {
   Container,
   ReaderContainer,
@@ -12,55 +11,24 @@ import {
   CloseIcon,
   FontSizeButton,
   ButtonWrapper
-} from "./Components";
+} from './Components'
 
-const storage = global.localStorage || null;
+const storage = global.localStorage || null
 
-const DEMO_URL =
-  "/react-reader/files/alice.epub";
-const DEMO_NAME = "Alice in wonderland";
+const DEMO_URL = '/react-reader/RLT/opf.opf'
+const DEMO_NAME = 'M. Scott Peck – The Road Less Traveled'
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-    margin: 0;
-    padding: 0;
-    color: inherit;
-    font-size: inherit;
-    font-weight: 300;
-    line-height: 1.4;
-    word-break: break-word;
-  }
-  html {
-    font-size: 62.5%;
-  }
-  body {
-    margin: 0;
-    padding: 0;
-    min-height: 100vh;
-    font-size: 1.8rem;
-    background: #333;
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    color: #fff;
-  }
-`;
-
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       fullscreen: false,
-      location:
-        storage && storage.getItem("epub-location")
-          ? storage.getItem("epub-location")
-          : 2,
+      location: storage?.getItem('epub-location') ?? 2,
       localFile: null,
       localName: null,
-      largeText: false
-    };
-    this.rendition = null;
+      largeText: false,
+    }
+    this.rendition = null
   }
 
   toggleFullscreen = () => {
@@ -70,61 +38,64 @@ class App extends Component {
       },
       () => {
         setTimeout(() => {
-          const evt = document.createEvent("UIEvents");
-          evt.initUIEvent("resize", true, false, global, 0);
-        }, 1000);
+          const evt = document.createEvent('UIEvents')
+          evt.initUIEvent('resize', true, false, global, 0)
+        }, 1000)
       }
-    );
-  };
+    )
+  }
 
-  onLocationChanged = location => {
+  onLocationChanged = (location) => {
     this.setState(
       {
         location
       },
       () => {
-        storage && storage.setItem("epub-location", location);
+        storage && storage.setItem('epub-location', location)
       }
-    );
-  };
+    )
+  }
 
   onToggleFontSize = () => {
-    const nextState = !this.state.largeText;
+    const nextState = !this.state.largeText
     this.setState(
       {
         largeText: nextState
       },
       () => {
-        this.rendition.themes.fontSize(nextState ? "140%" : "100%");
+        this.rendition.themes.fontSize(nextState ? '140%' : '100%')
       }
-    );
-  };
+    )
+  }
 
-  getRendition = rendition => {
+  getRendition = (rendition) => {
     console.log('getRendition callback', rendition)
     // Set inital font-size, and add a pointer to rendition for later updates
-    const { largeText } = this.state;
-    this.rendition = rendition;
-    rendition.themes.fontSize(largeText ? "140%" : "100%");
-  };
+    const { largeText } = this.state
+    this.rendition = rendition
+    rendition.themes.fontSize(largeText ? '140%' : '100%')
+  }
+
   handleChangeFile = (event, results) => {
     if (results.length > 0) {
-      const [e, file] = results[0];
-      if (file.type !== "application/epub+zip") {
-        return alert("Unsupported type");
+      const [e, file] = results[0]
+      if (file.type !== 'application/epub+zip') {
+        return alert('Unsupported type')
       }
       this.setState({
         localFile: e.target.result,
         localName: file.name,
         location: null
-      });
+      })
     }
-  };
+  }
+
   render() {
-    const { fullscreen, location, localFile, localName } = this.state;
+    const {
+      fullscreen, location, localFile, localName
+    } = this.state
     return (
       <Container>
-        <GlobalStyle />
         <Bar>
           <LogoWrapper href="https://github.com/gerhardsletten/react-reader">
             <Logo
@@ -155,8 +126,6 @@ class App extends Component {
           </FontSizeButton>
         </ReaderContainer>
       </Container>
-    );
+    )
   }
 }
-
-export default App;
